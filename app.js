fbf2bcf15346c7dc82e665f4cb62285e4e852877
gonzatdf <gonzatdf85@gmail.com>
2022-06-23 19:02:11 -0300

    dios me salve

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-const express = require('express');
-const path = require ("path");
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-
-app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname +'/views/index.html'))
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/views/index.html"));
 });
 
-app.get('/productDetail', (req,res)=>{
-    res.sendFile (path.resolve(__dirname, "views/productDetail.html"))
+app.get("/productDetail", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/productDetail.html"));
 });
 
-app.get('/productCart', (req,res) => {
-    res.sendFile(path.resolve(__dirname + '/views/productCart.html'))
+app.get("/productCart", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/views/productCart.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/register.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/login.html"));
 });
 
-app.listen (5000, () => console.log ('http://localhost:5000/'));
+app.listen(5000, () => console.log("http://localhost:5000/"));
