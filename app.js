e279f928d82701448ac634f540179694c72df1ba
LuHaidr <lucia.haidr@gmail.com>
2022-06-23 17:12:06 -0300

    intento de carrito

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const path = require ("path");
 const app = express();
 
 app.use(express.static('public'));
-const path = require ("path");
 
 
-app.get ('/', (req,res) => {
-    res.sendFile (path.resolve(__dirname, "views/index.html"))
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname +'/views/index.html'))
 });
 
+app.get('/productCart', (req,res) => {
+    res.sendFile(path.resolve(__dirname + '/views/productCart.html'))
+});
 
-
-
-app.listen (5000, () => console.log ("http://localhost:5000/"))
+app.listen (5000, () => console.log ('http://localhost:5000/'));
