f8f5a1d317f4b228608e952ec032965d38df3977
P1PEDUB <ufeldub@gmail.com>
2022-06-23 17:45:02 -0500

    resolvi conflictos app.js y modifique productDetail.css

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const express = require('express');
-const path = require ("path");
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-
-app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname +'/views/index.html'))
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/views/index.html"));
 });
 
-app.get('/productDetail', (req,res)=>{
-    res.sendFile (path.resolve(__dirname, "views/productDetail.html"))
+app.get("/productDetail", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views/productDetail.html"));
 });
 
-app.get('/productCart', (req,res) => {
-    res.sendFile(path.resolve(__dirname + '/views/productCart.html'))
+app.get("/productCart", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/views/productCart.html"));
 });
 
-app.listen (5000, () => console.log ('http://localhost:5000/'));
+app.listen(5000, () => console.log("http://localhost:5000/"));
