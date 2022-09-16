cb3c94c070e5aa8f5afa50eb5181b498914cb770
P1PEDUB <ufeldub@gmail.com>
2022-09-15 19:42:51 -0500

    Se realiza almacenamiento de usuarios en BD.

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,9 @@ const fs = require("fs");
 const path = require("path");
 const {validationResult} = require ("express-validator");
 const bcryptjs = require("bcryptjs");
+const db = require('../src/database/models');
+const Product = require("../src/database/models/Product");
+const sequelize = db.sequelize;
 
 const usersListPath = path.resolve(__dirname, "../database/users.json");
 const usersList = JSON.parse(fs.readFileSync(usersListPath, "utf8"));
@@ -15,61 +18,100 @@ let userController = {
   },
 
   processRegister: (req,res) => {
-    let userInDB = User.findByField("email", req.body.email);
-    if (userInDB) {
-      return res.render ("user/register.ejs", {
-        errors: {
-          email:{
-            msg :"Este email ya esta registrado"
-        }
-      },
-      oldData:req.body
-    });
-  }
-    const resultValidation = validationResult (req);
-    if (resultValidation.errors.length > 0) {
-      return res.render ("user/register",{ errors: resultValidation.mapped(), 
-      oldData: req.body}) //mapped: convertir el array a objeto
-    } else {
-    let newUser = req.body;
-    let avatar = req.file.filename;
-    newUser.id =  uuidv4();
-    newUser.avatar = avatar;
-    newUser.pass = bcryptjs.hashSync(req.body.pass,10); // encripta la contrasena
-    usersList.push (newUser);
-    fs.writeFileSync(usersListPath, JSON.stringify(usersList, null, 2));
-    res.redirect("/users/login");
-    }
+    //let userInDB = User.findByField("email", req.body.email);
+
+    db.User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(userInDB => {
+
+      if (userInDB) {
+        return res.render ("user/register.ejs", {
+          errors: {
+            email:{
+              msg :"Este email ya esta registrado"
+          }
+        },
+        oldData:req.body
+        });
+      }
+
+      const resultValidation = validationResult (req);
+      if (resultValidation.errors.length > 0) {
+        return res.render ("user/register",{ errors: resultValidation.mapped(), 
+        oldData: req.body}) //mapped: convertir el array a objeto
+      } else {
+
+        let newUser = req.body;
+        let avatar = req.file.filename;
+        let passSinCryp = req.body.pass;
+
+        //newUser.id =  uuidv4();
+        newUser.avatar = avatar;
+        newUser.pass = bcryptjs.hashSync(req.body.pass,10); // encripta la contrasena
+
+        db.User.create({
+          fullname: newUser.fullName,
+          email: newUser.email,
+          pass: newUser.pass,
+          pass_sin_cryp: passSinCryp,
+          avatar: newUser.avatar, 
+          admin: false
+        }).then(user => {
+          res.redirect("/users/login");
+        }).catch(error => {
+          console.log("Error al crear el usuario.", error);
+        })
+
+        /*
+        usersList.push (newUser);
+        fs.writeFileSync(usersListPath, JSON.stringify(usersList, null, 2));
+        */
+      }
+
+    }).catch(error => {
+      console.log("Error al obtener el usuario con email: " + req.body.email, error);
+    })
   },
   login: (req, res) => {
     res.render("user/login.ejs");
   },
   loginProcess: (req,res) => {
-    let userLogin = User.findByField("email", req.body.email);
-    if (userLogin) {
-      let passOk = bcryptjs.compareSync(req.body.pass, userLogin.pass)
-      if (passOk) {
-        delete userLogin.pass;
-        req.session.userLogged = userLogin;
-        res.redirect("/users/profile");
+
+    db.User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(userLogin => {
+
+      if (userLogin) {
+        let passOk = bcryptjs.compareSync(req.body.pass, userLogin.pass);
+        if (passOk) {
+          delete userLogin.pass;
+          req.session.userLogged = userLogin;
+          res.redirect("/users/profile");
+        }else{
+          return res.render ("user/login", {
+            errors: {
+              email: {
+                msg: "Las credenciales no son validas"
+              }
+            }
+            });
+        }
       }else{
         return res.render ("user/login", {
-          errors: {
-            email: {
-              msg: "Las credenciales no son validas"
-            }
+        errors: {
+          email: {
+            msg: "Email no esta registrado"
           }
-          });
-      }
-    }else{
-      return res.render ("user/login", {
-      errors: {
-        email: {
-          msg: "Email no esta registrado"
         }
+        });
       }
-      });
-    }
+    }).catch(error =>{
+      console.log("Error al intenatr obtener el usuario con email " + req.body.email);
+    })
   },
 
   profileView: (req, res) => {
